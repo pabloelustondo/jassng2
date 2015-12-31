cc486275722fd1c2aa34882b0c5fb2c582ec0e6e
Pablo Elustondo <pablo@jassbrain.com>
2015-12-30 22:36:53 -0500

    version 0.0 angular 2 hello world

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by pabloelustondo on 15-12-30.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'jassng2app',
+    template: '<h1>JassNg2 Version 0.0 Working</h1>'
+})
+export class AppComponent { }
---
+++ app/boot.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by pabloelustondo on 15-12-30.
+ */
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
