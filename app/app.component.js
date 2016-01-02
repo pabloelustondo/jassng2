26d7cb73dff82f51e460d0a219548a8589b7618f
Pablo Elustondo <pablo@jassbrain.com>
2016-01-01 22:53:04 -0500

    ngFor and styles

--- app/app.component.js
+++ app/app.component.js
@@ -12,16 +12,29 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var HEROES, AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
+                    this.heroes = HEROES;
                     this.hero = {
                         id: 1,
                         name: 'Windstorm1'
@@ -30,7 +43,7 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'jassng2app',
-                        template: "\n  <h1>{{title}}</h1>\n  <h2>{{hero.name}} details!</h2>\n  <div><label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n  </div>\n        "
+                        template: "\n    <h2>My Heroes</h2>\n<ul class=\"heroes\">\n  <li *ngFor=\"#hero of heroes\">\n  <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n  </li>\n</ul>\n  <h1>{{title}}</h1>\n  <h2>{{hero.name}} details!</h2>\n  <div><label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n  </div>\n        "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,19 @@
 
 import {Component} from 'angular2/core';
 
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
+
 interface Hero {
     id: number;
     name: string;
@@ -12,6 +25,12 @@ interface Hero {
 @Component({
     selector: 'jassng2app',
     template:`
+    <h2>My Heroes</h2>
+<ul class="heroes">
+  <li *ngFor="#hero of heroes">
+  <span class="badge">{{hero.id}}</span> {{hero.name}}
+  </li>
+</ul>
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
@@ -23,6 +42,7 @@ interface Hero {
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
+    public heroes = HEROES;
     public hero: Hero = {
         id: 1,
         name: 'Windstorm1'
