c903fd60f0173bac7b7ba37705a5b14a87a40af9
Pablo Elustondo <pablo@jassbrain.com>
2016-02-03 22:58:32 -0500

    part 3 done

--- app/app.component.js
+++ app/app.component.js
@@ -1,22 +1,27 @@
 /**
  * Created by pabloelustondo on 15-12-30.
  */
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', './herodetail.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+        switch (arguments.length) {
+            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+        }
     };
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var HEROES, AppComponent;
+    var core_1, herodetail_component_1;
+    var HEROES, STYLES, AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (herodetail_component_1_1) {
+                herodetail_component_1 = herodetail_component_1_1;
             }],
         execute: function() {
             HEROES = [
@@ -31,19 +36,19 @@ System.register(['angular2/core'], function(exports_1) {
                 { "id": 19, "name": "Magma" },
                 { "id": 20, "name": "Tornado" }
             ];
+            STYLES = ["\n\n  .heroesList{\n    float:left;\n    border:solid\n  }\n  .heroDetails{\n    border:solid\n  }\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 10em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0em;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .heroes li.selected:hover {\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #EEE;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0em 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0px 0px 4px;\n  }\n"];
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
                     this.heroes = HEROES;
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm1'
-                    };
                 }
+                AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'jassng2app',
-                        template: "\n    <h2>My Heroes</h2>\n<ul class=\"heroes\">\n  <li *ngFor=\"#hero of heroes\">\n  <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n  </li>\n</ul>\n  <h1>{{title}}</h1>\n  <h2>{{hero.name}} details!</h2>\n  <div><label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n  </div>\n        "
+                        directives: [herodetail_component_1.HeroDetailComponent],
+                        styles: STYLES,
+                        template: "\n<div class=\"heroesList\">\n<h2>My Heroes List</h2>\n<ul class=\"heroes\">\n  <li *ngFor=\"#hero of heroes\" (click)=\"onSelect(hero)\">\n  <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n  </li>\n</ul>\n</div>\n<herodetail [hero]=\"selectedHero\"></herodetail>\n        "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,8 @@
  */
 
 import {Component} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './herodetail.component';
 
 var HEROES: Hero[] = [
     { "id": 11, "name": "Mr. Nice" },
@@ -17,35 +19,84 @@ var HEROES: Hero[] = [
     { "id": 20, "name": "Tornado" }
 ];
 
-interface Hero {
-    id: number;
-    name: string;
-}
+var STYLES = [`
+
+  .heroesList{
+    float:left;
+    border:solid
+  }
+  .heroDetails{
+    border:solid
+  }
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 10em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0em;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #EEE;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0em 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0px 0px 4px;
+  }
+`]
 
 @Component({
     selector: 'jassng2app',
+    directives: [HeroDetailComponent],
+    styles: STYLES,
     template:`
-    <h2>My Heroes</h2>
+<div class="heroesList">
+<h2>My Heroes List</h2>
 <ul class="heroes">
-  <li *ngFor="#hero of heroes">
+  <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
   <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-  </div>
+</div>
+<herodetail [hero]="selectedHero"></herodetail>
         `
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
     public heroes = HEROES;
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm1'
-    };
+    public selectedHero: Hero;
+
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 
 }
